9e9b2dc06ec8c92f401d126e13849c234e3088d1
gayathri <gayathri.machavarapu@capgemini.com>
2023-01-03 08:53:06 +0530

    changed

--- src/main/java/com/cg/osa/service/ICustomerServiceImpl.java
+++ src/main/java/com/cg/osa/service/ICustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.cg.osa.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -61,24 +62,20 @@ public class ICustomerServiceImpl implements ICustomerService{
  		  throw new CustomerException("Id not found");
  	  }
    }
+   public List<CustomerDTO> viewAllCustomerByCity(String city) throws CustomerException{
+		  List<CustomerDTO> list = customerRepository.findAll();
+		  List<CustomerDTO> custCity = list.stream().filter(customerDTO->customerDTO.getAddress().getCity().equals(city)).collect(Collectors.toList());
+		  if(custCity.size()!=0) {
+		   return custCity;
+	   }else {
+			  throw new CustomerException("no customer from the city "+city);
+	   }
+	   }       
   
    
-   public List<CustomerDTO> viewAllCustomerByCity(String city) throws CustomerException{
-	  List<CustomerDTO> list= customerRepository.findAll();
-	  List<CustomerDTO> custCity = new ArrayList<CustomerDTO>();
-	  for(int i=0;i<list.size();i++) {
-		  CustomerDTO cust=list.get(i);
-		  if(city.equals(cust.getAddress().getCity())) {
-			 custCity.add(cust); 
-		  }
-		  
-	  }
-	  if(custCity.size()!=0) {
-	   return custCity;
-   }else {
-		  throw new CustomerException("no customer from the city "+city);
-   }
-   }
+   
+	  
+   
 	       
 
 }
